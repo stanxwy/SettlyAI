67e3929a38dacd4228f1fa017b8b43548693b3ac
Miley Liu <smileyliu325@gmail.com>
2025-07-30 15:36:33 +1000

    feat: fetch report and save

--- frontend/src/pages/Suburb/SuburbReportPage.tsx
+++ frontend/src/pages/Suburb/SuburbReportPage.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import type { AppDispatch, RootState } from '@/store';
+import { fetchSuburbReport } from '@/store/slices/suburbSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useLocation } from 'react-router-dom';
 
 type SuburbReportPageProps = {
   location: string;
   suburbId: string;
 };
-const SuburbReportPage: React.FC<SuburbReportPageProps> = ({location}) => {
- 
+const SuburbReportPage: React.FC<SuburbReportPageProps> = ({ location }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { suburbId, report, loading, error } = useSelector(
+    (state: RootState) => state.suburb
+  );
 
-   const { state } = useLocation() as { state: { suburbId?: string } };
+  useEffect(() => {
+    if (suburbId) {
+      dispatch(fetchSuburbReport(suburbId));
+    }
+  }, [suburbId, dispatch]);
 
-  console.log("location",location);
-  console.log("suburb",state.suburbId);
- 
-
-  if (!location || !location.includes('+')) {
-    return <div>Invalid URL</div>;
-  }
-  if(!state.suburbId) {
-    return <div>suburb not found</div>
-  }
+  if (loading) return <p>Loading report...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!report) return <p>No report found.</p>;
 
   const [stateCode, suburbName] = location.split('+');
-
 
   return (
     <div>
       <h2>State: {stateCode}</h2>
       <h3>Suburb: {suburbName}</h3>
-      <h3>SuburbID:{state.suburbId}</h3>
+      <h3>SuburbID:{suburbId}</h3>
+      <pre>{JSON.stringify(report, null, 2)}</pre>
     </div>
   );
 };
