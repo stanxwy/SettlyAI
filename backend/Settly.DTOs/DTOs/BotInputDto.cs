abb8eb9fcfa1ef7b34959cefb89e364a0d53ba35
cccarlos2727 <cccarlos2727@gmail.com>
2025-08-02 23:44:04 +1000

    refator: update Dto attribute

--- backend/Settly.DTOs/DTOs/BotInputDto.cs
+++ backend/Settly.DTOs/DTOs/BotInputDto.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Settly.DTOs.DTOs
