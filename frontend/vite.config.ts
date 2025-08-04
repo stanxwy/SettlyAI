283fa2df706ee8cede8011f022382484ad165fdd
Miley Liu <smileyliu325@gmail.com>
2025-08-04 13:02:16 +1000

    use pnpx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -17,5 +26,31 @@ export default defineConfig({
     environment: 'jsdom',
     // setupFiles: ["./src/test/setup.ts"],
     reporters: ['verbose'],
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+          setupFiles: ['.storybook/vitest.setup.ts'],
+        },
+      },
+    ],
   },
 });
