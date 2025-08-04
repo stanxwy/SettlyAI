c19e2f5c2cea44a48346dfedb97dd7ba657d059e
cccarlos2727 <cccarlos2727@gmail.com>
2025-08-04 13:16:30 +1000

    refactor: Move the Dtos folder to Project folder SettlyModels

--- backend/SettlyModels/DTOs/BotOutputDto.cs
+++ backend/SettlyModels/DTOs/BotOutputDto.cs
@@ -2,7 +2,7 @@ namespace Settly.DTOs
 {
     public class BotOutputDto
     {
-        public string Response { get; set; } = string.Empty;        
+        public string Response { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
--- backend/SettlyModels/DTOs/SearchInputDto.cs
+++ backend/SettlyModels/DTOs/SearchInputDto.cs
@@ -6,7 +6,7 @@ namespace Settly.DTOs
     public class SearchInputDto
     {
         [FromQuery(Name = "q")]
-        [Required(ErrorMessage = "Your input can't be empty")]        
+        [Required(ErrorMessage = "Your input can't be empty")]
         [MaxLength(40, ErrorMessage = "Your input is too long.")]
         [MinLength(3, ErrorMessage = "Your input is invalid.")]
         public string Query { get; set; } = string.Empty;
