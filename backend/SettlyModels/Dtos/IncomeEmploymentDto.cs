6b9369c5a0b890673fa8f23bc017a06ad37c8568
ShatterBloom <pamtatech@gmail.com>
2025-08-07 22:45:53 +1000

    update dto: align with entity and use decimal

--- backend/SettlyModels/Dtos/IncomeEmploymentDto.cs
+++ backend/SettlyModels/Dtos/IncomeEmploymentDto.cs
@@ -3,8 +3,8 @@ namespace SettlyModels.Dtos;
 
 public class IncomeEmploymentDto
 {
-    public float MedianIncome { get; set; }
-    public float EmploymentRate { get; set; }
-    public float WhiteCollarRatio { get; set; }
-    public float JobGrowthRate { get; set; }
+    public int MedianIncome { get; set; }
+    public decimal EmploymentRate { get; set; }
+    public decimal WhiteCollarRatio { get; set; }
+    public decimal JobGrowthRate { get; set; }
 }
--- backend/SettlyModels/Dtos/PopulationSupplyDto.cs
+++ backend/SettlyModels/Dtos/PopulationSupplyDto.cs
@@ -9,12 +9,8 @@ namespace SettlyModels.Dtos;
 public class PopulationSupplyDto
 {
     public int SuburbId { get; set; }
-    public int Population { get; set; }
-    public float PopulationGrowthRate { get; set; }
-    public float RentersRatio { get; set; }
+    public decimal RentersRatio { get; set; }
     public decimal DemandSupplyRatio { get; set; }
-    public string LandSupply { get; set; } = string.Empty;
-    public int DevProjectsCount { get; set; }
     public int BuildingApprovals12M { get; set; }
-
+    public int DevProjectsCount { get; set; }
 }
