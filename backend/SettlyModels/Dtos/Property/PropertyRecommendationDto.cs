acf717f4868f351754fac92c6c2735b25ac512f3
Miley Liu <smileyliu325@gmail.com>
2025-08-15 17:39:49 +1000

    feat: add dto

---
+++ backend/SettlyModels/Dtos/Property/PropertyRecommendationDto.cs
@@ -0,0 +1,14 @@
+namespace SettlyModels.Dtos;
+
+
+public class PropertyRecommendationDto
+{
+    public int Id { get; set; }
+    public string ImageUrl { get; set; } = String.Empty;
+
+    public int Price { get; set; }
+    public string Address { get; set; } = String.Empty;
+    public int Bedrooms { get; set; }
+    public int Bathrooms { get; set; }
+    public int Carpark { get; set; }
+}
