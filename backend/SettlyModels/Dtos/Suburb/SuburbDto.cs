da95e6d343c07178a4bef28ecad20d145991630b
shelton <damwcs@gmail.com>
2025-08-21 14:05:27 +1000

    merge from main

--- backend/SettlyModels/Dtos/Suburb/SuburbDto.cs
+++ backend/SettlyModels/Dtos/Suburb/SuburbDto.cs
@@ -7,15 +7,4 @@ public class SuburbDto
     public string State { get; set; } = null!;
     public string Postcode { get; set; } = null!;
     public string SuburbName { get; set; } = null!;
-
-    public IncomeEmploymentDto? IncomeEmployment { get; set; }
-    public HousingMarketDto? HousingMarket { get; set; }
-    public PopulationSupplyDto? PopulationSupply { get; set; }
-    public RiskDevelopmentDto? RiskDevelopment { get; set; }
-    public LivabilityDto? Livability { get; set; }
-    public SettlyAIScoreDto? SettlyAIScore { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-
 }
