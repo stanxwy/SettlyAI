0a4166c82b06d826a9db85aa3661b3c97e500536
shelton <damwcs@gmail.com>
2025-08-21 16:57:45 +1000

    modify dtobug

--- backend/SettlyModels/Dtos/Suburb/SuburbDto.cs
+++ backend/SettlyModels/Dtos/Suburb/SuburbDto.cs
@@ -2,9 +2,8 @@ namespace SettlyModels.Dtos;
 
 public class SuburbDto
 {
-    public string Id { get; set; } = string.Empty;
     public int SuburbId { get; set; }
     public string State { get; set; } = null!;
     public string Postcode { get; set; } = null!;
-    public string SuburbName { get; set; } = null!;
+    public string Name { get; set; } = null!;
 }
